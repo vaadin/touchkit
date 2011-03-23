47a21ade1251271f35668aa7b4466ccd273ae6aa
Matti Tahvonen <matti.tahvonen@itmill.com>
2011-03-23 13:36:52 +0000

    constructor with caption
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@17893 de684a11-831f-0410-ab27-cb86445eb682

--- src/main/java/com/vaadin/addons/touchkit/ui/OptionLayout.java
+++ src/main/java/com/vaadin/addons/touchkit/ui/OptionLayout.java
@@ -7,12 +7,18 @@ import com.vaadin.ui.CssLayout;
  * corners to visualize grouping.
  * <p>
  * TODO may be better to create a customized client side counterpart. Or should
- * this actually be a styled FormLayout? See eg. ios settings and something with text field.
+ * this actually be a styled FormLayout? See eg. ios settings and something with
+ * text field.
  */
 public class OptionLayout extends CssLayout {
 
 	public OptionLayout() {
 		setStyleName("v-optionlayout");
 		setWidth("100%");
+	}
+
+	public OptionLayout(String caption) {
+		this();
+		setCaption(caption);
 	}
 }
