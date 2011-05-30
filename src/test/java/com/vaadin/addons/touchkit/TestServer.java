6e0e815b645f2221fbc3205e0ee62a443d4766cd
Matti Tahvonen <matti.tahvonen@itmill.com>
2011-05-30 15:23:41 +0000

    * gwt 2.3 update  * TestServer now needs no web.xml file, configured programmatically.
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@19146 de684a11-831f-0410-ab27-cb86445eb682

--- src/test/java/com/vaadin/addons/touchkit/TestServer.java
+++ src/test/java/com/vaadin/addons/touchkit/TestServer.java
@@ -5,7 +5,10 @@ import java.io.File;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
+
+import com.vaadin.addons.touchkit.server.TouchKitApplicationServlet;
 
 public class TestServer {
 
@@ -18,27 +21,36 @@ public class TestServer {
 	 */
 	public static void main(String[] args) throws Exception {
 		Server server = new Server();
-		
+
 		final Connector connector = new SelectChannelConnector();
 
 		connector.setPort(7777);
 		server.setConnectors(new Connector[] { connector });
 
-        WebAppContext context = new WebAppContext();
-        context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
+		WebAppContext context = new WebAppContext();
+
+		ServletHolder servletHolder = new ServletHolder(
+				TouchKitApplicationServlet.class);
+		servletHolder.setInitParameter("widgetset",
+				"com.vaadin.addons.touchkit.gwt.TouchKitWidgetSet");
+		servletHolder.setInitParameter("application",
+				TouchKitTestApp.class.getName());
+
 		File file = new File("./target");
 		File[] listFiles = file.listFiles();
 		for (File file2 : listFiles) {
-			if(file2.isDirectory() && file2.getName().startsWith("vaadin-touchkit-")) {
+			if (file2.isDirectory()
+					&& file2.getName().startsWith("vaadin-touchkit-")) {
 				context.setWar(file2.getPath());
 				break;
 			}
 		}
-        context.setContextPath("/");
- 
-        server.setHandler(context);
-        
-        server.start();
+		context.setContextPath("/");
+		context.addServlet(servletHolder, "/*");
+
+		server.setHandler(context);
+
+		server.start();
 		server.join();
 	}
 
