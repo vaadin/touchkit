6033ab8772c10c24dad5ea3f0b85aff05559e437
Marc Englund <marc.englund@itmill.com>
2011-06-07 13:44:47 +0000

    Styles for TouchKitWindow popover window ("ipad mode"), for #7078
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@19267 de684a11-831f-0410-ab27-cb86445eb682

--- src/main/java/com/vaadin/addons/touchkit/gwt/client/TouchKitResources.java
+++ src/main/java/com/vaadin/addons/touchkit/gwt/client/TouchKitResources.java
@@ -10,45 +10,48 @@ import com.google.gwt.resources.client.ImageResource.ImageOptions;
 import com.google.gwt.resources.client.ImageResource.RepeatStyle;
 
 public interface TouchKitResources extends ClientBundle {
-	public static final TouchKitResources INSTANCE = GWT
-			.create(TouchKitResources.class);
+    public static final TouchKitResources INSTANCE = GWT
+            .create(TouchKitResources.class);
 
-	/**
-	 * TODO consider splitting this per components/related components
-	 * @return
-	 */
-	@NotStrict
-	@Source("touchkit.css")
-	public CssResource css();
+    /**
+     * TODO consider splitting this per components/related components
+     * 
+     * @return
+     */
+    @NotStrict
+    @Source("touchkit.css")
+    public CssResource css();
 
-	@Source("back_button.png")
-	public DataResource backButtonImage();
+    @Source("back_button.png")
+    public DataResource backButtonImage();
 
-	@Source("navbar-action.png")
-	public DataResource navBarActionImage();
+    @Source("navbar-action.png")
+    public DataResource navBarActionImage();
 
-	@Source("background_stripes.png")
-	public DataResource backgroundStripesImage();
+    @Source("background_stripes.png")
+    public DataResource backgroundStripesImage();
 
-	@Source("header_middle.png")
-	@ImageOptions(repeatStyle = RepeatStyle.Horizontal)
-	public ImageResource toolbarBackground();
+    @Source("header_middle.png")
+    @ImageOptions(repeatStyle = RepeatStyle.Horizontal)
+    public ImageResource toolbarBackground();
 
-	@Source("slider-handle.png")
-	@ImageOptions(repeatStyle = RepeatStyle.None)
-	public ImageResource sliderHandle();
+    @Source("slider-handle.png")
+    @ImageOptions(repeatStyle = RepeatStyle.None)
+    public ImageResource sliderHandle();
 
-	@Source("modalWindowArrow.png")
-	@ImageOptions(repeatStyle = RepeatStyle.None)
-	public ImageResource modalWindowArrow();
+    @Source("modalWindowArrow.png")
+    @ImageOptions(repeatStyle = RepeatStyle.None)
+    public ImageResource modalWindowArrow();
 
-	@Source("modalWindowArrowDown.png")
-	@ImageOptions(repeatStyle = RepeatStyle.None)
-	public ImageResource modalWindowArrowDown();
-	
-	@Source("switch.png")
-	@ImageOptions(repeatStyle = RepeatStyle.None)
-	public ImageResource switchImage();
+    @Source("modalWindowArrowDown.png")
+    @ImageOptions(repeatStyle = RepeatStyle.None)
+    public ImageResource modalWindowArrowDown();
 
+    @Source("switch.png")
+    @ImageOptions(repeatStyle = RepeatStyle.None)
+    public ImageResource switchImage();
+
+    @Source("popupBackgroundWithToolbar.png")
+    public DataResource popupWithToolbarImage();
 
 }
