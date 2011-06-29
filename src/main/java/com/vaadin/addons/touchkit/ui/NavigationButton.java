701af3f3c84f27dbcd7ffc483a3519d969a54d16
Marc Englund <marc.englund@itmill.com>
2011-06-29 13:07:31 +0000

    Javadocced NavigationButton
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@19577 de684a11-831f-0410-ab27-cb86445eb682

--- src/main/java/com/vaadin/addons/touchkit/ui/NavigationButton.java
+++ src/main/java/com/vaadin/addons/touchkit/ui/NavigationButton.java
@@ -10,98 +10,143 @@ import com.vaadin.ui.ClientWidget;
 import com.vaadin.ui.Component;
 
 /**
- * A Button implementation optimized to be used inside a {@link NavigationManager}
- * .
+ * A {@link Button} implementation optimized to be used inside a
+ * {@link NavigationManager}.
  * <p>
- * Clicking button will automatically navigate to next view if one is defined in
- * this button (via constructor or {@link #setTargetView(Component)}). On the
- * client side, NavigationManager will start animation immediately when clicked.
+ * Clicking button will automatically navigate to the target view if one is
+ * defined in this button (via constructor or {@link #setTargetView(Component)}
+ * ). On the client side, the {@link NavigationManager} will start animation
+ * immediately when clicked, while the server request/response has finished.
+ * <p>
+ * Note that navigation will only work when the button is used inside a
+ * {@link NavigationManager}, otherwise it will work as a regular {@link Button}.
  */
 @ClientWidget(VNavigationButton.class)
 public class NavigationButton extends Button {
-	private Component targetView;
+    private Component targetView;
 
-	public NavigationButton(String caption) {
-		super(caption);
-	}
+    /**
+     * @see Button#Button(String)
+     */
+    public NavigationButton(String caption) {
+        super(caption);
+    }
 
-	public NavigationButton(Component targetView) {
-		super(targetView.getCaption());
-		this.targetView = targetView;
-	}
+    /**
+     * Constructs a button with the specified target view, and sets the caption
+     * to equal that of the target view.
+     * 
+     * @param targetView
+     *            the view to navigate to when pressed
+     */
+    public NavigationButton(Component targetView) {
+        super(targetView.getCaption());
+        this.targetView = targetView;
+    }
 
-	public NavigationButton() {
-	}
+    /**
+     * @see Button#Button()
+     */
+    public NavigationButton() {
+    }
 
-	// @Override
-	// protected void fireClick() {
-	// /*
-	// * TODO remove debug timeout
-	// */
-	// // try {
-	// // Thread.sleep(300);
-	// // } catch (InterruptedException e) {
-	// // // TODO Auto-generated catch block
-	// // e.printStackTrace();
-	// // }
-	// if (targetView != null) {
-	// getNavigationPanel().navigateTo(targetView);
-	// }
-	// super.fireClick();
-	// }
+    // @Override
+    // protected void fireClick() {
+    // /*
+    // * TODO remove debug timeout
+    // */
+    // // try {
+    // // Thread.sleep(300);
+    // // } catch (InterruptedException e) {
+    // // // TODO Auto-generated catch block
+    // // e.printStackTrace();
+    // // }
+    // if (targetView != null) {
+    // getNavigationPanel().navigateTo(targetView);
+    // }
+    // super.fireClick();
+    // }
 
-	@Override
-	public void changeVariables(Object source, Map<String, Object> variables) {
-		// FIXME since #6605 click implementation in Button sucks. Fix it then
-		// uncomment the previous method or something.
-		// super.changeVariables(source, variables);
-		if (targetView != null) {
-			getNavigationPanel().navigateTo(targetView);
-		}
-		fireClick();
-	}
+    @Override
+    public void changeVariables(Object source, Map<String, Object> variables) {
+        // FIXME since #6605 click implementation in Button sucks. Fix it then
+        // uncomment the previous method or something.
+        // super.changeVariables(source, variables);
+        if (targetView != null) {
+            getNavigationManager().navigateTo(targetView);
+        }
+        fireClick();
+    }
 
-	/**
-	 * @return the navigation panel in which the button is contained or null if
-	 *         the button is not inside a {@link NavigationManager}
-	 */
-	public NavigationManager getNavigationPanel() {
-		Component p = getParent();
-		while (p != null && !(p instanceof NavigationManager)) {
-			p = p.getParent();
-		}
-		return (NavigationManager) p;
-	}
+    /**
+     * Gets the @link {@link NavigationManager} in which this button is
+     * contained.
+     * 
+     * @return the {@link NavigationManager} or null if not inside one
+     */
+    public NavigationManager getNavigationManager() {
+        Component p = getParent();
+        while (p != null && !(p instanceof NavigationManager)) {
+            p = p.getParent();
+        }
+        return (NavigationManager) p;
+    }
 
-	@Override
-	public void paintContent(PaintTarget target) throws PaintException {
-		super.paintContent(target);
-		if (targetView != null && targetView.getApplication() != null) {
-			target.addAttribute("nv", targetView);
-		}
-	}
+    @Override
+    public void paintContent(PaintTarget target) throws PaintException {
+        super.paintContent(target);
+        if (targetView != null && targetView.getApplication() != null) {
+            target.addAttribute("nv", targetView);
+        }
+    }
 
-	public void setTargetView(Component targetView) {
-		this.targetView = targetView;
-		requestRepaint();
-	}
+    /**
+     * Sets the view that will be navigated to when the button is pressed.
+     * <p>
+     * The button communicates directly with a @link {@link NavigationManager}
+     * to make the actual navigation work.
+     * </p>
+     * 
+     * @param targetView
+     */
+    public void setTargetView(Component targetView) {
+        this.targetView = targetView;
+        requestRepaint();
+    }
 
-	@Override
-	public String getCaption() {
-		String caption2 = super.getCaption();
-		if(caption2 == null) {
-			/*
-			 * Use caption from target view unless explicitly set for this button 
-			 */
-			if (targetView != null && targetView.getCaption() != null) {
-				return targetView.getCaption();
-			}
-		}
-		return caption2;
-	}
+    /**
+     * Gets the caption for this button.
+     * <p>
+     * If the caption is explicitly set, it will be used - otherwise the caption
+     * is fetched from the target view, if one is set.
+     * </p>
+     * 
+     * @see com.vaadin.ui.AbstractComponent#getCaption()
+     */
+    @Override
+    public String getCaption() {
+        String caption2 = super.getCaption();
+        if (caption2 == null) {
+            /*
+             * Use caption from target view unless explicitly set for this
+             * button
+             */
+            if (targetView != null && targetView.getCaption() != null) {
+                return targetView.getCaption();
+            }
+        }
+        return caption2;
+    }
 
-	public Component getTargetView() {
-		return targetView;
-	}
+    /**
+     * Gets the target view that will be navigated to when the button is
+     * pressed.
+     * 
+     * @return the current target view
+     * @see #setTargetView(Component)
+     */
+    public Component getTargetView() {
+        return targetView;
+    }
 
 }
