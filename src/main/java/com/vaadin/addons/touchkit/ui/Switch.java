f1dbd618696ba741d00ca987144a44e2c6a64bf8
Marc Englund <marc.englund@itmill.com>
2011-06-30 13:37:35 +0000

    Javadocced and added a full set of constructors (sans the evil one that we do not mention)
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@19601 de684a11-831f-0410-ab27-cb86445eb682

--- src/main/java/com/vaadin/addons/touchkit/ui/Switch.java
+++ src/main/java/com/vaadin/addons/touchkit/ui/Switch.java
@@ -1,15 +1,52 @@
 package com.vaadin.addons.touchkit.ui;
 
 import com.vaadin.addons.touchkit.gwt.client.VSwitch;
+import com.vaadin.data.Property;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.ClientWidget;
 
 /**
- * A checkbox rendered as a switch button like in ios.
+ * A {@link CheckBox} that is rendered as a switch button, which might provide a
+ * better user experience on touch devices.
  * 
  * @see CheckBox
  */
 @ClientWidget(VSwitch.class)
 public class Switch extends CheckBox {
+
+    /**
+     * @see CheckBox#CheckBox()
+     */
+    public Switch() {
+        super();
+    }
+
+    /**
+     * @see CheckBox#CheckBox(String,boolean)
+     */
+    public Switch(String caption, boolean initialState) {
+        super(caption, initialState);
+    }
+
+    /**
+     * @see CheckBox#CheckBox(String,ClickListener)
+     */
+    public Switch(String caption, ClickListener listener) {
+        super(caption, listener);
+    }
+
+    /**
+     * @see CheckBox#CheckBox(String,Property)
+     */
+    public Switch(String caption, Property dataSource) {
+        super(caption, dataSource);
+    }
+
+    /**
+     * @see CheckBox#CheckBox(String)
+     */
+    public Switch(String caption) {
+        super(caption);
+    }
 
 }
