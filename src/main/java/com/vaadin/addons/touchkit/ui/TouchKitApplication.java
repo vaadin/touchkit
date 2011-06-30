253172a065b484acd5a0dce363ca805f6fb208ab
Marc Englund <marc.englund@itmill.com>
2011-06-30 13:49:48 +0000

    Checked javadoc, and made setMainWindow() throw if argument is not TouchKitWindow
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@19603 de684a11-831f-0410-ab27-cb86445eb682

--- src/main/java/com/vaadin/addons/touchkit/ui/TouchKitApplication.java
+++ src/main/java/com/vaadin/addons/touchkit/ui/TouchKitApplication.java
@@ -14,25 +14,25 @@ import com.vaadin.ui.Window;
 
 /**
  * TouchKitApplication is a specialized {@link Application} implementation to
- * help building Vaadin applications designed specifically for various devices.
- * It provides quick access to {@link WebBrowser} object via
+ * help building Vaadin applications designed specifically for various touch
+ * devices. It provides quick access to the {@link WebBrowser} object via
  * {@link #getBrowser()} and adds a specific method to do real application setup
  * in a phase when all information in WebBrowser is available via
  * {@link #onBrowserDetailsReady()}.
  * <p>
- * Helper also provides static getter method for TouchKitApplication aka thread
- * local pattern.
+ * Helper also provides static getter method for TouchKitApplication a.k.a
+ * thread local pattern.
  * <p>
- * The standard Vaadin application init process calls {@link #init()} method
+ * The standard Vaadin application init process calls the {@link #init()} method
  * very early. In that phase data all browser details are not necessary known by
- * the server side. So instead of overriding the init method as usual,
- * developers should override onBrowserDetailsReady() method in case their UI
- * depends on details detected via {@link WebBrowser}. The init method
- * implementation sets a TouchKitWindow as a main window and does the required
- * magic to call {@link #onBrowserDetailsReady()} when JavaScript detected
- * details from the client have been received. On that phase the main window
- * also has window width and heigth set according to the data provided by the
- * client side code.
+ * the server side, so instead of overriding the init method as usual,
+ * developers should override {@link #onBrowserDetailsReady()} method in case
+ * their UI depends on details detected via {@link WebBrowser}. The init method
+ * implementation sets a {@link TouchKitWindow} as main window and does the
+ * required magic to call {@link #onBrowserDetailsReady()} when JavaScript
+ * detected details from the client have been received. On that phase the main
+ * window also has window width and height set according to the data provided by
+ * the client side code.
  * <p>
  * Note, that only {@link TouchKitWindow}s are supported as top level windows.
  */
@@ -63,6 +63,17 @@ public abstract class TouchKitApplication extends Application implements
      * the main window.
      */
     public abstract void onBrowserDetailsReady();
+
+    @Override
+    public void setMainWindow(Window mainWindow) {
+        if (mainWindow instanceof TouchKitWindow) {
+            super.setMainWindow(mainWindow);
+        } else {
+            throw new IllegalArgumentException("Only "
+                    + TouchKitWindow.class.getSimpleName()
+                    + " can be set as main window");
+        }
+    }
 
     @Override
     public TouchKitWindow getMainWindow() {
