f72eceb10a2a3e684746cceb9b434f93ea75455a
Matti Tahvonen <matti.tahvonen@itmill.com>
2011-09-01 10:34:00 +0000

    work in progress
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@20786 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-integration-tests/src/test/java/com/vaadin/addon/itest/SmokeITCase.java
+++ vaadin-touchkit-integration-tests/src/test/java/com/vaadin/addon/itest/SmokeITCase.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.URL;
 
 import junit.framework.Assert;
@@ -20,7 +19,7 @@ public class SmokeITCase {
     @Test
     public void smokeTest() throws IOException {
 
-        URL url = new URL("http://localhost:8080/");
+        URL url = new URL("http://localhost:5678/");
 
         InputStream openStream = url.openStream();
 
