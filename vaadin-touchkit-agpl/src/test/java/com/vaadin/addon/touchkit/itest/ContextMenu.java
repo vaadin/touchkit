ae0ad2f54bd08d8434ae8b5aec29661b80c2e0ad
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-04-10 13:52:42 +0000

    test case  for #8302
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@23424 de684a11-831f-0410-ab27-cb86445eb682

---
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/ContextMenu.java
@@ -0,0 +1,53 @@
+package com.vaadin.addon.touchkit.itest;
+
+import com.vaadin.addon.touchkit.ui.NavigationManager;
+import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.addon.touchkit.ui.TouchKitWindow;
+import com.vaadin.event.Action;
+import com.vaadin.event.Action.Handler;
+import com.vaadin.ui.Table;
+
+public class ContextMenu extends TouchKitWindow {
+
+    public ContextMenu() {
+
+        NavigationManager navigationManager = new NavigationManager();
+        setContent(navigationManager);
+
+        NavigationView iv = new NavigationView();
+        iv.setCaption("Table with context menu");
+
+        final Table table = new Table();
+        table.addContainerProperty("foo", String.class, "foo");
+        table.addContainerProperty("bar", String.class, "bar");
+        table.addContainerProperty("car", String.class, "car");
+        table.setRowHeaderMode(Table.ROW_HEADER_MODE_INDEX);
+
+        table.addActionHandler(new Handler() {
+
+            private Action[] actions = new Action[] { new Action("Copy"),
+                    new Action("Paste") };
+
+            public void handleAction(Action action, Object sender, Object target) {
+
+                table.getWindow().showNotification("Just test");
+            }
+
+            public Action[] getActions(Object target, Object sender) {
+                return actions;
+            }
+        });
+
+        table.addItem();
+        table.addItem();
+        table.addItem();
+
+        table.setSizeFull();
+
+        iv.setContent(table);
+
+        navigationManager.setCurrentComponent(iv);
+
+    }
+
+}
