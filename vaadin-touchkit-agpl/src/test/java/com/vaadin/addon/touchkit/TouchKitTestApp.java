f98477fe3dee1568920dab80ae862cc47fef347a
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-08-08 09:26:27 +0000

    clicking test now opens it
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24113 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/TouchKitTestApp.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/TouchKitTestApp.java
@@ -6,6 +6,10 @@ import java.util.Collection;
 import java.util.Random;
 
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.terminal.Page;
 import com.vaadin.terminal.Resource;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.terminal.WrappedRequest;
@@ -224,11 +228,16 @@ public class TouchKitTestApp extends Root {
         // setTheme("base");
 
         String requestPathInfo = request.getRequestPathInfo();
-        if(requestPathInfo.length() > 3) {
+        if (requestPathInfo.length() > 3) {
             try {
 
-                String className = getClass().getPackage().getName() + ".itest."
-                        + requestPathInfo.substring(1);
+                String className;
+                if (requestPathInfo.startsWith("/com.")) {
+                    className = requestPathInfo.substring(1);
+                } else {
+                    className = getClass().getPackage().getName() + ".itest."
+                            + requestPathInfo.substring(1);
+                }
                 Class<?> forName = Class.forName(className);
                 if (forName != null) {
                     CssLayout newInstance = (CssLayout) forName.newInstance();
@@ -237,7 +246,7 @@ public class TouchKitTestApp extends Root {
                 return;
             } catch (ClassNotFoundException e) {
                 // TODO Auto-generated catch block
-                 e.printStackTrace();
+                e.printStackTrace();
             } catch (InstantiationException e) {
                 // TODO Auto-generated catch block
                 // e.printStackTrace();
@@ -251,24 +260,29 @@ public class TouchKitTestApp extends Root {
                 "TouchKit test server, get test by adding its name to url, eg: Selects or subpackage.FooBar");
         addComponent(label);
         addComponent(new Label("TODO add list of available tests here"));
-        
-        File itestroot = new File("src/test/java/" + getClass().getPackage().getName().replace(".", "/") + "/itest");
-        
+
+        File itestroot = new File("src/test/java/"
+                + getClass().getPackage().getName().replace(".", "/")
+                + "/itest");
+
         Collection<Class<? extends AbstractTouchKitIntegrationTest>> tests = new ArrayList<Class<? extends AbstractTouchKitIntegrationTest>>();
         addTests(getClass().getPackage().getName() + ".itest", itestroot, tests);
-        
+
         Table table = new Table();
-        BeanItemContainer<Class<? extends AbstractTouchKitIntegrationTest>> beanItemContainer = new BeanItemContainer<Class<? extends AbstractTouchKitIntegrationTest>>(tests);
+        BeanItemContainer<Class<? extends AbstractTouchKitIntegrationTest>> beanItemContainer = new BeanItemContainer<Class<? extends AbstractTouchKitIntegrationTest>>(
+                tests);
         table.setContainerDataSource(beanItemContainer);
-        table.setVisibleColumns(new Object[]{"canonicalName"});
+        table.setVisibleColumns(new Object[] { "canonicalName" });
         table.addGeneratedColumn("description", new ColumnGenerator() {
-            public Object generateCell(Table source, Object itemId, Object columnId) {
+            public Object generateCell(Table source, Object itemId,
+                    Object columnId) {
                 Class<?> c = (Class<?>) itemId;
                 try {
-                    AbstractTouchKitIntegrationTest t = (AbstractTouchKitIntegrationTest) c.newInstance();
+                    AbstractTouchKitIntegrationTest t = (AbstractTouchKitIntegrationTest) c
+                            .newInstance();
                     Label label2 = new Label(t.getDescription());
-//                    label2.setWidth("300px");
-//                    label2.setHeight("50px");
+                    // label2.setWidth("300px");
+                    // label2.setHeight("50px");
                     return label2;
                 } catch (InstantiationException e) {
                     // TODO Auto-generated catch block
@@ -280,6 +294,14 @@ public class TouchKitTestApp extends Root {
                 return null;
             }
         });
+        table.addListener(new ItemClickListener() {
+
+            public void itemClick(ItemClickEvent event) {
+                Class<?> itemId = (Class<?>) event.getItemId();
+                String canonicalName = itemId.getCanonicalName();
+                Page.getCurrent().open(new ExternalResource(canonicalName));
+            }
+        });
         addComponent(table);
 
     }
@@ -288,25 +310,26 @@ public class TouchKitTestApp extends Root {
             Collection<Class<? extends AbstractTouchKitIntegrationTest>> tests) {
         File[] listFiles = itestroot.listFiles();
         for (File file : listFiles) {
-            if(file.isDirectory()) {
+            if (file.isDirectory()) {
                 addTests(base + "." + file.getName(), file, tests);
-            } else if(file.getName().endsWith(".java")) {
-                String name = file.getName().substring(0, file.getName().indexOf("."));
+            } else if (file.getName().endsWith(".java")) {
+                String name = file.getName().substring(0,
+                        file.getName().indexOf("."));
                 try {
                     Class<?> forName = Class.forName(base + "." + name);
-                    
-                    if(AbstractTouchKitIntegrationTest.class.isAssignableFrom(forName)) {
+
+                    if (AbstractTouchKitIntegrationTest.class
+                            .isAssignableFrom(forName)) {
                         tests.add((Class<? extends AbstractTouchKitIntegrationTest>) forName);
                     }
                 } catch (ClassNotFoundException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-                
+
             }
-            
+
         }
-        
-        
+
     }
 }
