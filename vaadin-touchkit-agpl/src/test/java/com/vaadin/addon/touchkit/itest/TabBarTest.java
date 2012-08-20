82da8b84e76086915a97250b9da35e5bff696674
Samuli Penttilä <samuli.penttila@vaadin.com>
2012-08-20 09:15:29 +0000

    test tab bar with icon, text and icon with text. tab bar text alignment fixed. TBD: focused icon have ugly margin.
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24198 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/TabBarTest.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/TabBarTest.java
@@ -2,6 +2,7 @@ package com.vaadin.addon.touchkit.itest;
 
 import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
 import com.vaadin.addon.touchkit.ui.TabBarView;
+import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TabSheet.Tab;
 
@@ -13,9 +14,17 @@ public class TabBarTest extends AbstractTouchKitIntegrationTest {
         for (int loop = 0; loop < 5; loop++) {
             String content = String.format("Tab%d", loop);
             Label label = new Label(content);
-            label.setCaption(content);
+            // label.setCaption(content);
             Tab tab = bar.addTab(label);
-            tab.setCaption(content);
+
+            if (loop % 3 == 0) {
+                tab.setIcon(new ThemeResource("../runo/icons/32/folder.png"));
+                tab.setCaption(content);
+            } else if (loop % 3 == 1) {
+                tab.setCaption(content);
+            } else {
+                tab.setIcon(new ThemeResource("../runo/icons/32/folder.png"));
+            }
         }
 
         addComponent(bar);
