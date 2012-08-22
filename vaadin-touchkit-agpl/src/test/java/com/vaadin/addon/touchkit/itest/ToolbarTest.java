61e298f135270dea5639673753804aff48e464b4
Samuli Penttilä <samuli.penttila@vaadin.com>
2012-08-22 11:04:13 +0000

    basic use test case for toolbar
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24219 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/ToolbarTest.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/ToolbarTest.java
@@ -4,18 +4,31 @@ import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
 import com.vaadin.addon.touchkit.ui.Toolbar;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
 
 public class ToolbarTest extends AbstractTouchKitIntegrationTest {
     public ToolbarTest() {
         setDescription("This is Toolbar test");
         Toolbar tbar = new Toolbar();
+        final Label label = new Label();
+        label.setValue("Clicked: none");
 
         for (int loop = 0; loop < 10; loop++) {
             Button button = new Button();
             button.setIcon(new ThemeResource("../runo/icons/32/folder.png"));
+            final int identifier = loop;
+            button.addListener(new Button.ClickListener() {
+
+                @Override
+                public void buttonClick(ClickEvent event) {
+                    label.setValue("Clicked: " + identifier);
+                }
+            });
             tbar.addComponent(button);
         }
 
         addComponent(tbar);
+        addComponent(label);
     }
 }
