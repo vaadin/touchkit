0f2f2117747141521050a12cb75a3e9de02e7ac6
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-08-29 15:40:43 +0000

    #9310 Improved TabBarViewTest to contain common use cases.
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24246 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/TabBarTest.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/TabBarTest.java
@@ -1,7 +1,9 @@
 package com.vaadin.addon.touchkit.itest;
 
 import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
+import com.vaadin.addon.touchkit.itest.oldtests.TestUtils;
 import com.vaadin.addon.touchkit.ui.TabBarView;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -12,6 +14,8 @@ public class TabBarTest extends AbstractTouchKitIntegrationTest {
     private Tab lastTab = null;
 
     public TabBarTest() {
+        
+        TestUtils.injectCss(".green {background-color: green;}");
         setDescription("This is TabBar test");
 
         final TabBarView bar = new TabBarView();
@@ -49,6 +53,23 @@ public class TabBarTest extends AbstractTouchKitIntegrationTest {
             } else {
                 tab.setIcon(new ThemeResource("../runo/icons/32/folder.png"));
             }
+            
+            if(loop == 0) {
+                label.setSizeFull();
+                label.addStyleName("green");
+                label.setValue(label.getValue() + ", green color should fill the whole content area");
+            } else if (loop == 1) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(label.getValue());
+                sb.append("Content on this tab should be scrollable");
+                for (int i = 0; i < 200; i++) {
+                    sb.append("Row " + i + "</br>");
+                }
+                label.setContentMode(ContentMode.XHTML);
+                label.setValue(sb.toString());
+            }
+            
+            
 
             lastTab = tab;
         }
