364ca7248912a693e081d75294ad058ccb4086fb
Jonatan Kronqvist <jonatan.kronqvist@itmill.com>
2012-09-10 07:31:26 +0000

    XHTML -> HTML
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24354 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/TabBarTest.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/TabBarTest.java
@@ -3,8 +3,8 @@ package com.vaadin.addon.touchkit.itest;
 import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
 import com.vaadin.addon.touchkit.itest.oldtests.TestUtils;
 import com.vaadin.addon.touchkit.ui.TabBarView;
-import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.server.ThemeResource;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
@@ -14,7 +14,7 @@ public class TabBarTest extends AbstractTouchKitIntegrationTest {
     private Tab lastTab = null;
 
     public TabBarTest() {
-        
+
         TestUtils.injectCss(".green {background-color: green;}");
         setDescription("This is TabBar test");
 
@@ -53,11 +53,12 @@ public class TabBarTest extends AbstractTouchKitIntegrationTest {
             } else {
                 tab.setIcon(new ThemeResource("../runo/icons/32/folder.png"));
             }
-            
-            if(loop == 0) {
+
+            if (loop == 0) {
                 label.setSizeFull();
                 label.addStyleName("green");
-                label.setValue(label.getValue() + ", green color should fill the whole content area");
+                label.setValue(label.getValue()
+                        + ", green color should fill the whole content area");
             } else if (loop == 1) {
                 StringBuilder sb = new StringBuilder();
                 sb.append(label.getValue());
@@ -65,11 +66,9 @@ public class TabBarTest extends AbstractTouchKitIntegrationTest {
                 for (int i = 0; i < 200; i++) {
                     sb.append("Row " + i + "</br>");
                 }
-                label.setContentMode(ContentMode.XHTML);
+                label.setContentMode(ContentMode.HTML);
                 label.setValue(sb.toString());
             }
-            
-            
 
             lastTab = tab;
         }
