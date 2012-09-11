b30697fa74ba403250f69fd65c5543c8edf1d39a
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-09-11 07:17:52 +0000

    Less deprecated method to hook to app initialization
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24366 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/server/TouchKitServlet.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/server/TouchKitServlet.java
@@ -1,19 +1,28 @@
 package com.vaadin.addon.touchkit.server;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
-import com.vaadin.addon.touchkit.ui.TouchKitSettings;
+import com.vaadin.addon.touchkit.rootextensions.TouchKitSettings;
+import com.vaadin.server.AddonContext;
+import com.vaadin.server.ApplicationStartedEvent;
+import com.vaadin.server.ApplicationStartedListener;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.server.VaadinServletSession;
-import com.vaadin.server.WrappedHttpServletRequest;
 
 public class TouchKitServlet extends VaadinServlet {
-        
+
     @Override
-    protected void onVaadinSessionStarted(WrappedHttpServletRequest request,
-            VaadinServletSession session) throws ServletException {
-        TouchKitSettings.init(session);
-        super.onVaadinSessionStarted(request, session);
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+        AddonContext addonContext2 = getVaadinService().getAddonContext();
+        addonContext2
+                .addApplicationStartedListener(new ApplicationStartedListener() {
+
+                    @Override
+                    public void applicationStarted(ApplicationStartedEvent event) {
+                        TouchKitSettings.init(event.getApplication());
+                    }
+                });
     }
 
 }
