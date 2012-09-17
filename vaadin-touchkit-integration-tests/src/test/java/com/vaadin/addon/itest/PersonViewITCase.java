3cddbbb777b6e9b4edddd739de9460c4e833784c
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-09-17 08:34:12 +0000

    fixed person view tests
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24443 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-integration-tests/src/test/java/com/vaadin/addon/itest/PersonViewITCase.java
+++ vaadin-touchkit-integration-tests/src/test/java/com/vaadin/addon/itest/PersonViewITCase.java
@@ -1,5 +1,12 @@
 package com.vaadin.addon.itest;
 
+import com.vaadin.addon.touchkit.itest.oldtests.PersonView;
+
 public class PersonViewITCase extends AbstractRenderingTest {
+    
+    @Override
+    protected String getTestViewName() {
+        return PersonView.class.getName();
+    }
 
 }
