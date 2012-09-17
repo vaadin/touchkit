19ac1c96e77b3d81d529045ce4b6a484a230c021
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-09-17 08:33:55 +0000

    fixed select tests
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24442 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-integration-tests/src/test/java/com/vaadin/addon/itest/SelectsITCase.java
+++ vaadin-touchkit-integration-tests/src/test/java/com/vaadin/addon/itest/SelectsITCase.java
@@ -1,5 +1,12 @@
 package com.vaadin.addon.itest;
 
+import com.vaadin.addon.touchkit.itest.oldtests.Selects;
+
 public class SelectsITCase extends AbstractRenderingTest {
+    
+    @Override
+    protected String getTestViewName() {
+        return Selects.class.getName();
+    }
 
 }
