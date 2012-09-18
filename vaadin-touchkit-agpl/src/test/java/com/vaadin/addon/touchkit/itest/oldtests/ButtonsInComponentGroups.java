11b043a51d7ef17dcffd28762d6c289f6fb216ed
Johannes Tuikkala <johannes.tuikkala@itmill.com>
2012-09-18 07:39:59 +0000

    #9234 css fixes for verticalcomponentgroup
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24450 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/oldtests/ButtonsInComponentGroups.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/oldtests/ButtonsInComponentGroups.java
@@ -6,11 +6,12 @@ import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
 import com.vaadin.addon.touchkit.ui.HorizontalComponentGroup;
 import com.vaadin.addon.touchkit.ui.NavigationView;
 import com.vaadin.addon.touchkit.ui.VerticalComponentGroup;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 public class ButtonsInComponentGroups extends AbstractTouchKitIntegrationTest {
 
@@ -18,74 +19,76 @@ public class ButtonsInComponentGroups extends AbstractTouchKitIntegrationTest {
         NavigationView navigationView = new NavigationView();
         navigationView.setCaption("Buttons in various places");
 
-        CssLayout l = new CssLayout();
-//        l.setMargin(true);
-        
+        VerticalLayout l = new VerticalLayout();
+        l.setMargin(true);
+        l.setMargin(new MarginInfo(false, false, false, true));
+
         Button button = new Button("Button not in a component group");
         button.setWidth("100%");
         l.addComponent(button);
 
         VerticalComponentGroup verticalComponentGroup = new VerticalComponentGroup(
                 "Vertical component group");
-//        verticalComponentGroup.setMargin(false);
-        
+        verticalComponentGroup.setWidth("100%");
+
         button = new Button("Button");
         button.setWidth("100%");
         verticalComponentGroup.addComponent(button);
-        
+
         verticalComponentGroup.addComponent(new TextField("TextField"));
-        
+
         button = new Button("Button too");
         button.setWidth("100%");
         verticalComponentGroup.addComponent(button);
-        
-        verticalComponentGroup.addComponent(new Label("FIXME: Label, between buttons bugs"));
-        
+
+        verticalComponentGroup.addComponent(new Label(
+                "FIXME: Label, between buttons bugs"));
+
         button = new Button("Button too");
         button.setWidth("100%");
         verticalComponentGroup.addComponent(button);
 
         l.addComponent(verticalComponentGroup);
-        
+
         verticalComponentGroup = new VerticalComponentGroup();
-//        verticalComponentGroup.setMargin(false);
+        verticalComponentGroup.setWidth("100%");
         verticalComponentGroup.setCaption("Horizontal in vertical");
         HorizontalComponentGroup horizontalGroup = getHorizontalGroup();
         horizontalGroup.setCaption("Caption");
         verticalComponentGroup.addComponent(horizontalGroup);
-        
-        
+
         horizontalGroup = getHorizontalGroup();
         horizontalGroup.addComponent(new Button("Third"));
         horizontalGroup.setWidth("300px");
-        Iterator<Component> componentIterator = horizontalGroup.getComponentIterator();
-        while(componentIterator.hasNext()) {
+        Iterator<Component> componentIterator = horizontalGroup
+                .getComponentIterator();
+        while (componentIterator.hasNext()) {
             Component next = componentIterator.next();
-            next.setWidth(""
-                + 100.0/((double)horizontalGroup.getComponentCount()) + "%");
+            next.setWidth("" + 100.0 / (horizontalGroup.getComponentCount())
+                    + "%");
         }
         verticalComponentGroup.addComponent(horizontalGroup);
-        
+
         horizontalGroup = getHorizontalGroup();
         Iterator<Component> it = horizontalGroup.getComponentIterator();
         it.next().setCaption("Only one here");
         horizontalGroup.removeComponent(it.next());
         verticalComponentGroup.addComponent(horizontalGroup);
-        
+
         l.addComponent(verticalComponentGroup);
-        
+
         l.addComponent(getHorizontalGroup());
 
         navigationView.setContent(l);
-        
+
         HorizontalComponentGroup horizontalComponentGroup = new HorizontalComponentGroup();
         horizontalComponentGroup.addComponent(new Button("Up"));
         horizontalComponentGroup.addComponent(new Button("Down"));
         navigationView.setRightComponent(horizontalComponentGroup);
         horizontalComponentGroup.setCaption("Horizontal straight to layout");
-        
+
         navigationView.setLeftComponent(new Button("Left"));
-        
+
         addComponent(navigationView);
         TestUtils.makeSmallTabletSize(navigationView);
 
@@ -95,7 +98,7 @@ public class ButtonsInComponentGroups extends AbstractTouchKitIntegrationTest {
         HorizontalComponentGroup horizontalComponentGroup = new HorizontalComponentGroup();
         horizontalComponentGroup.addComponent(new Button("First"));
         horizontalComponentGroup.addComponent(new Button("Another"));
-        
+
         return horizontalComponentGroup;
     }
 
