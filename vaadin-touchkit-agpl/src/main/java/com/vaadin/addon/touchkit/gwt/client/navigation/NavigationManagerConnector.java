1eb905d1761d311d215d97419160ea824ed73b88
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-09-20 15:35:22 +0000

    enabled rendering suspension during animation, removed obsolete old code (commented out)
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@24511 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/navigation/NavigationManagerConnector.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/navigation/NavigationManagerConnector.java
@@ -89,133 +89,14 @@ public class NavigationManagerConnector extends
                 getWidget().getElement(), listener);
     }
 
-    //
-    // public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
-    // // ac = (VTouchKitApplicationConnection) client;
-    // rendering = true;
-    // // this.client = client;
-    // if (client.updateComponent(this, uidl, true)) {
-    // rendering = false;
-    // return;
-    // }
-    //
-    // doUpdate(uidl);
-    //
-    // verifyPositions.trigger();
-    //
-    // }
-    //
-    // private void updatePaintable(Paintable paintable, UIDL childUIDL) {
-    // int widgetIndex = getWidgetIndex((Widget) paintable);
-    // if (widgetIndex == -1) {
-    // // new widget, attach
-    // add((Widget) paintable, createContainerElement());
-    // }
-    // paintable.updateFromUIDL(childUIDL, client);
-    //
-    // }
-    //
-    //
-    // // client.updateVariable(client.getPid(this), "navigated", -1, true);
-    //
-    //
-    // private void doUpdate(UIDL uidl) {
-    // // final ArrayList<Widget> orphanedPaintables = new ArrayList<Widget>();
-    // // for (Widget w : getChildren()) {
-    // // if (w instanceof Paintable) {
-    // // orphanedPaintables.add(w);
-    // // }
-    // // }
-    // // /*
-    // // * First render visible component. We'll lazy render others but the
-    // next
-    // // * visible.
-    // // */
-    // // int childCount = uidl.getChildCount();
-    // // for (int i = 0; i < childCount; i++) {
-    // // UIDL childUIDL = uidl.getChildUIDL(i);
-    // // Paintable paintable = client.getPaintable(childUIDL);
-    // // orphanedPaintables.remove(paintable);
-    // // }
-    // // final Paintable newNext = uidl.getPaintableAttribute("n", client);
-    // // Paintable newCurrent = uidl.getPaintableAttribute("c", client);
-    // // final Paintable newPrev = uidl.getPaintableAttribute("p", client);
-    // // updatePaintable(newCurrent, getChildUidl(newCurrent, uidl));
-    // // if (newCurrent == currentView) {
-    // // /*
-    // // * already at correct position due to NavigationButtonClick -> no
-    // // * transition.
-    // // */
-    // // } else if (prevView == newCurrent) {
-    // // /*
-    // // * Back navigation, slide right then ensure positions.
-    // // */
-    // // slideFromLeft();
-    // // } else if (currentView == null) {
-    // // /*
-    // // * Placeholder or initial rendering.
-    // // */
-    // // setPosition(newCurrent, -currentWrapperPos);
-    // // } else {
-    // // /*
-    // // * Forward navigation, slide left. First ensure newNext is on the
-    // // * right side.
-    // // */
-    // // slideFromRight(newCurrent);
-    // // }
-    // //
-    // // final UIDL newNextUidl = getChildUidl(newNext, uidl);
-    // // final UIDL newPrevUidl = getChildUidl(newPrev, uidl);
-    // //
-    // // currentView = newCurrent;
-    // // nextView = newNext;
-    // // prevView = newPrev;
-    // //
-    // // /*
-    // // * Detach orphaned components. Must be eagerly done so that orphaned
-    // // * components don't send variables anymore.
-    // // */
-    // //
-    // // for (Widget widget : orphanedPaintables) {
-    // // com.google.gwt.dom.client.Element wrapperElement = widget
-    // // .getElement().getParentElement();
-    // // widget.removeFromParent();
-    // // client.unregisterPaintable((Paintable) widget);
-    // // wrapper.removeChild(wrapperElement);
-    // // }
-    // //
-    // // if (newNext != null) {
-    // // updatePaintable(newNext, newNextUidl);
-    // // setPosition(newNext, -currentWrapperPos + 1);
-    // // }
-    // // if (newPrev != null) {
-    // // updatePaintable(newPrev, newPrevUidl);
-    // // setPosition(newPrev, -currentWrapperPos - 1);
-    // // }
-    // //
-    // // /**
-    // // * Stylesheet fades in new navigation views in 150ms. After they have
-    // // * become visible, remove placeholder below them.
-    // // */
-    // // new Timer() {
-    // // @Override
-    // // public void run() {
-    // // hidePlaceHolder();
-    // // }
-    // // }.schedule(160);
-    // //
-    // // rendering = false;
-    //
-    // }
-
     @Override
     public void animationWillStart() {
-        // getConnection().suspendRendering(this);
+        getConnection().suspendRendering(this);
     }
 
     @Override
     public void animationDidEnd() {
-        // getConnection().resumeRendering(this);
+        getConnection().resumeRendering(this);
     }
 
 }
