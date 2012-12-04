9aba411e9453fb2480b03e10f9eec7177b3127ca
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-12-04 12:19:05 +0000

    trying to hotfix styles since base them refactoring in V7 nightlies
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25149 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/VEagerResourceLoader.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/VEagerResourceLoader.java
@@ -1,14 +1,18 @@
 package com.vaadin.addon.touchkit.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class VEagerResourceLoader implements EntryPoint {
+
+    private static final String TOUCHKIT_STYLE_NAME = "v-tk";
 
     public void onModuleLoad() {
         VTouchKitResources.INSTANCE.css().ensureInjected();
         if (isHighDPI()) {
             VTouchKitResources.INSTANCE.highDpiCss().ensureInjected();
         }
+        RootPanel.getBodyElement().addClassName(TOUCHKIT_STYLE_NAME);
     }
 
     public static native boolean isHighDPI()
