c334230f37c58a59ca8b26a54587d265959a2d6c
Jonatan Kronqvist <jonatan.kronqvist@itmill.com>
2012-12-04 09:03:33 +0000

    Offline mode now correctly caches the widgetset in the client side application cache
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25146 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/rootextensions/ApplicationCacheSettings.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/rootextensions/ApplicationCacheSettings.java
@@ -1,6 +1,11 @@
 package com.vaadin.addon.touchkit.rootextensions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import com.vaadin.server.BootstrapFragmentResponse;
 import com.vaadin.server.BootstrapListener;
@@ -23,9 +28,42 @@ public class ApplicationCacheSettings extends AbstractTouchKitRootExtension
     public void modifyBootstrapPage(BootstrapPageResponse response) {
         Document document = response.getDocument();
         if (isCacheManifestEnabled()) {
-             document.getElementsByTag("html").attr("manifest",
-             getCacheManifestLocation(response));
+            document.getElementsByTag("html").attr("manifest",
+                    getCacheManifestLocation(response));
+
+            // Add the widgetsetUrl parameter to the bootstrap parameters.
+            Element scriptTag = document.getElementsByTag("script").last();
+            String script = scriptTag.html();
+            String vaadinDir = getAppConfigParameter("vaadinDir", script);
+            String widgetset = getAppConfigParameter("widgetset", script);
+            String widgetsetUrl = String.format(
+                    "%swidgetsets/%s/%s.nocache.js", vaadinDir, widgetset,
+                    widgetset);
+
+            scriptTag.html("");
+            scriptTag.appendChild(new DataNode(script.replace("\n});", String
+                    .format(",\n    \"widgetsetUrl\": \"%s\"\n});",
+                            widgetsetUrl)), scriptTag.baseUri()));
         }
+    }
+
+    /**
+     * Parses parameters from a JSON block of vaadin initialization parameters.
+     * 
+     * @param parameter
+     *            the parameter to get the value for
+     * @param script
+     *            the entire contents of the script tag holding the vaadin app
+     *            initialization parameters.
+     * @return the value of the parameter
+     */
+    private String getAppConfigParameter(String parameter, String script) {
+        Matcher m = Pattern.compile(
+                String.format(".*\"%s\": \"(.*)\"", parameter)).matcher(script);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return null;
     }
 
     private String getCacheManifestLocation(BootstrapPageResponse response) {
