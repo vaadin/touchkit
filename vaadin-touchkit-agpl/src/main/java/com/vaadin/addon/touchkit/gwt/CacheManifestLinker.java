faf0b3cda3352535b02499a1c8812627ddb5921a
Sami Viitanen <sami.viitanen@vaadin.com>
2013-01-29 13:40:09 +0000

    Ticket #10669 - Multiple manifests support from 2.x.
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25427 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/CacheManifestLinker.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/CacheManifestLinker.java
@@ -2,11 +2,16 @@ package com.vaadin.addon.touchkit.gwt;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
 import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.regex.Pattern;
 
 import org.reflections.Reflections;
@@ -21,20 +26,28 @@ import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.linker.AbstractLinker;
 import com.google.gwt.core.ext.linker.Artifact;
 import com.google.gwt.core.ext.linker.ArtifactSet;
+import com.google.gwt.core.ext.linker.CompilationResult;
 import com.google.gwt.core.ext.linker.ConfigurationProperty;
+import com.google.gwt.core.ext.linker.EmittedArtifact;
 import com.google.gwt.core.ext.linker.LinkerOrder;
 import com.google.gwt.core.ext.linker.SelectionProperty;
-import com.google.gwt.core.ext.linker.SyntheticArtifact;
+import com.google.gwt.core.ext.linker.Shardable;
+
 
 /**
  * TODO review and improve this impl
  */
 @LinkerOrder(LinkerOrder.Order.POST)
+@Shardable
 public class CacheManifestLinker extends AbstractLinker {
 
-    private HashSet<String> cachedArtifacts = new HashSet<String>();
+    private final HashSet<String> cachedArtifacts = new HashSet<String>();
 
-    private HashSet<String> onlineArtifacts = new HashSet<String>();
+    private static Set<String> allArtifacts = Collections
+            .synchronizedSet(new HashSet<String>());
+
+    private static Map<String, Set<String>> generatedManifestResources = Collections
+            .synchronizedMap(new HashMap<String, Set<String>>());
 
     public CacheManifestLinker() {
         addCachedResource("/");
@@ -46,13 +59,13 @@ public class CacheManifestLinker extends AbstractLinker {
         // Find resources using org.reflections
         Reflections reflections = new Reflections(
                 new ConfigurationBuilder()
-                        .filterInputsBy(
-                                new FilterBuilder()
-                                        .include("VAADIN\\.themes\\.base.*"))
+                .filterInputsBy(
+                        new FilterBuilder()
+                        .include("VAADIN\\.themes\\.base.*"))
                         .setUrls(
                                 ClasspathHelper
-                                        .forPackage("VAADIN.themes.base"))
-                        .setScanners(new ResourcesScanner()));
+                                .forPackage("VAADIN.themes.base"))
+                                .setScanners(new ResourcesScanner()));
 
         Set<String> themeFiles = reflections.getResources(Pattern
                 .compile("^.*\\.(css|gif|png|ico)$"));
@@ -68,62 +81,107 @@ public class CacheManifestLinker extends AbstractLinker {
     }
 
     @Override
+    @SuppressWarnings("rawtypes")
     public ArtifactSet link(TreeLogger logger, LinkerContext context,
-            ArtifactSet artifacts) throws UnableToCompleteException {
+            ArtifactSet artifacts, boolean onePermutation)
+                    throws UnableToCompleteException {
+
+        loadTouchKitWidgetSetResources(context);
+
         ArtifactSet newArtifacts = new ArtifactSet(artifacts);
 
-        SortedSet<ConfigurationProperty> configurationProperties = context
-                .getConfigurationProperties();
-        for (ConfigurationProperty configurationProperty : configurationProperties) {
-            if (configurationProperty.getName().equals(
-                    "touchkit.manifestlinker.additionalCacheRoot")) {
-                List<String> values = configurationProperty.getValues();
-                for (String root : values) {
-                    addResourcesRecursively(root);
-                }
-                break;
-            }
-        }
-
-        /*
-         * TODO make manifest for all permutations as we widen browsers support.
-         * Even IE 10 is supposed to support cache manifests. The servlet then
-         * needs to serve the correct one based on useragent.
-         */
-
-        for (Artifact<?> artifact : newArtifacts) {
-            for (SelectionProperty property : context.getProperties()) {
-                if (property.getName().equals("user.agent")) {
-                    String value = property.tryGetValue();
-                    if (value != null && value.equals("safari")) {
-                        addCachedResource(artifact);
+        if (onePermutation) {
+            String userAgent = "";
+            for (CompilationResult result : artifacts
+                    .find(CompilationResult.class)) {
+                SortedSet<SortedMap<SelectionProperty, String>> propertiesMap = result
+                        .getPropertyMap();
+                for (SortedMap<SelectionProperty, String> sm : propertiesMap) {
+                    Set<SelectionProperty> keySet = sm.keySet();
+                    for (SelectionProperty selectionProperty : keySet) {
+                        if ("user.agent".equals(selectionProperty.getName())) {
+                            userAgent = sm.get(selectionProperty);
+                            break;
+                        }
+                    }
+                    if (userAgent != null) {
                         break;
                     }
                 }
+                if (userAgent != null) {
+                    break;
+                }
             }
+
+            if (userAgent == null || userAgent.equals("")) {
+                // If only one permutation, expect safari
+                // TODO fetch real value
+                userAgent = "safari";
+            }
+
+            SortedSet<String> hashSet = new TreeSet<String>();
+
+            for (Artifact artifact : artifacts) {
+                if (artifact instanceof EmittedArtifact) {
+                    EmittedArtifact ea = (EmittedArtifact) artifact;
+                    String pathName = ea.getPartialPath();
+                    if (acceptCachedResource(pathName)) {
+                        hashSet.add(pathName);
+                        allArtifacts.add(pathName);
+                    }
+                }
+            }
+
+            generatedManifestResources.put(userAgent, hashSet);
+        } else {
+
+            for (Artifact artifact : artifacts) {
+                if (artifact instanceof EmittedArtifact) {
+                    EmittedArtifact ea = (EmittedArtifact) artifact;
+                    String pathName = ea.getPartialPath();
+                    if (acceptCachedResource(pathName)) {
+                        if (!allArtifacts.contains(pathName)) {
+                            // common stuff like kickstart script, included
+                            // scripts, styles, images etc..
+                            cachedArtifacts.add(pathName);
+                        }
+                    }
+                }
+            }
+
+            Set<String> keySet = generatedManifestResources.keySet();
+            for (String ua : keySet) {
+                Set<String> set = generatedManifestResources.get(ua);
+                set.addAll(cachedArtifacts);
+                newArtifacts.add(createCacheManifest(context, logger, set, ua));
+            }
+
         }
-
-        String moduleName = context.getModuleName();
-
-        addCachedResource(moduleName + ".nocache.js");
-
-        StringBuilder cm = new StringBuilder();
-        cm.append("CACHE MANIFEST\n");
-        cm.append("# Build time" + new Date());
-        cm.append("\n\nCACHE:\n");
-
-        for (String fn : cachedArtifacts) {
-            cm.append(fn);
-            cm.append("\n");
-        }
-        cm.append("\nNETWORK:\n");
-        cm.append("*\n\n");
-
-        SyntheticArtifact cacheManifest = emitString(logger, cm.toString(),
-                getCacheManifestFileName());
-        newArtifacts.add(cacheManifest);
 
         return newArtifacts;
+    }
+
+    /**
+     * Traverses directories specified in gwt modules to be added to cache
+     * manifests. E.g. themes.
+     * 
+     * @param context
+     */
+    private void loadTouchKitWidgetSetResources(LinkerContext context) {
+        synchronized (cachedArtifacts) {
+            SortedSet<ConfigurationProperty> configurationProperties = context
+                    .getConfigurationProperties();
+            for (ConfigurationProperty configurationProperty : configurationProperties) {
+                if (configurationProperty.getName().equals(
+                        "touchkit.manifestlinker.additionalCacheRoot")) {
+                    List<String> values = configurationProperty.getValues();
+                    for (String root : values) {
+                        addResourcesRecursively(root);
+                    }
+                    break;
+                }
+            }
+        }
     }
 
     private void addResourcesRecursively(String root) {
@@ -183,6 +241,37 @@ public class CacheManifestLinker extends AbstractLinker {
 
     protected String getCacheManifestFileName() {
         return "cache.manifest";
+    }
+
+    private Artifact<?> createCacheManifest(LinkerContext context,
+            TreeLogger logger, Set<String> artifacts, String userAgent)
+                    throws UnableToCompleteException {
+
+        StringBuilder cm = new StringBuilder();
+        cm.append("CACHE MANIFEST\n");
+        cm.append("# Build time" + new Date());
+        cm.append("\n\nCACHE:\n");
+
+        for (String fn : artifacts) {
+            cm.append(fn);
+            cm.append("\n");
+        }
+        cm.append("\nNETWORK:\n");
+        cm.append("*\n\n");
+
+        String manifest = cm.toString();
+
+        // using cache.manifest for webkits as ancient android and ios versions
+        // might bug with other names
+        String prefix;
+        if (userAgent.equals("safari")) {
+            prefix = "";
+        } else {
+            prefix = userAgent + ".";
+        }
+
+        String manifestName = prefix + "cache.manifest";
+        return emitString(logger, manifest, manifestName);
     }
 
 }
