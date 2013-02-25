7cb26e274fe965acc644fa4001d4d52d379bcb51
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-02-25 14:07:59 +0000

    Started collecting style variations to constants #11146
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25629 de684a11-831f-0410-ab27-cb86445eb682

---
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/theme/StyleNames.java
@@ -0,0 +1,10 @@
+package com.vaadin.addon.touchkit.gwt.client.theme;
+
+/**
+ * This class contains special style variations available in TouchKits theme.
+ */
+public interface StyleNames {
+    public static final String BUTTON_RED = "red";
+    public static final String BUTTON_GREEN = "green";
+    public static final String BUTTON_BLUE = "blue";
+}
