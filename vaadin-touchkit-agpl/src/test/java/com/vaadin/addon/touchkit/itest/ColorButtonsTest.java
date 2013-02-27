6100207c03d606fc8cb3b89579d87eb4fa3c3705
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-02-27 21:31:53 +0000

    made colored button test more versatile
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25647 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/ColorButtonsTest.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/ColorButtonsTest.java
@@ -2,24 +2,57 @@ package com.vaadin.addon.touchkit.itest;
 
 import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
 import com.vaadin.addon.touchkit.gwt.client.theme.StyleNames;
+import com.vaadin.addon.touchkit.ui.HorizontalButtonGroup;
+import com.vaadin.addon.touchkit.ui.NavigationView;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.VerticalLayout;
 
 public class ColorButtonsTest extends AbstractTouchKitIntegrationTest {
 
     public ColorButtonsTest() {
+        
+        NavigationView navigationView = new NavigationView("RGB");
+        
+        VerticalLayout content = new VerticalLayout();
+        content.setSpacing(true);
+        content.setMargin(true);
+        
+        addButtons(content);
+        
+        HorizontalButtonGroup horizontalButtonGroup = new HorizontalButtonGroup();
+        
+        addButtons(horizontalButtonGroup);
+        
+        content.addComponent(horizontalButtonGroup);
+        
+        navigationView.setContent(content);
+        addComponent(navigationView);
+        
+        horizontalButtonGroup = new HorizontalButtonGroup();
+        addButtons(horizontalButtonGroup);
+        navigationView.setRightComponent(horizontalButtonGroup);
+        
+        Button red = new Button("Red");
+        red.addStyleName(StyleNames.BUTTON_RED);
+        navigationView.setLeftComponent(red);
+        
+    }
+
+    private void addButtons(ComponentContainer content) {
         Button normal = new Button("Normal");
-        addComponent(normal);
+        content.addComponent(normal);
 
         Button red = new Button("Red");
         red.addStyleName(StyleNames.BUTTON_RED);
-        addComponent(red);
+        content.addComponent(red);
 
         Button green = new Button("Green");
         green.addStyleName(StyleNames.BUTTON_GREEN);
-        addComponent(green);
+        content.addComponent(green);
 
         Button blue = new Button("Blue");
         blue.addStyleName(StyleNames.BUTTON_BLUE);
-        addComponent(blue);
+        content.addComponent(blue);
     }
 }
