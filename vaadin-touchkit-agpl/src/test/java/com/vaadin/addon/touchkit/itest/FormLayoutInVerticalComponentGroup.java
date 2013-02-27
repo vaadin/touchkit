45ef591fa6c66c9ab7a60c6215995c915dbf6c59
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-02-27 22:09:57 +0000

    fixes #10956, formlayout css tuning. Css still bloated at this part, but works somewhat ok.
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25649 de684a11-831f-0410-ab27-cb86445eb682

---
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/FormLayoutInVerticalComponentGroup.java
@@ -0,0 +1,44 @@
+package com.vaadin.addon.touchkit.itest;
+
+import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
+import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.addon.touchkit.ui.Switch;
+import com.vaadin.addon.touchkit.ui.VerticalComponentGroup;
+import com.vaadin.server.UserError;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.themes.BaseTheme;
+
+public class FormLayoutInVerticalComponentGroup extends AbstractTouchKitIntegrationTest {
+    public FormLayoutInVerticalComponentGroup() {
+        
+        NavigationView navigationView = new NavigationView("Form");
+        
+        setDescription("This is a test for FormLayou in VCGt");
+        
+        FormLayout formLayout = new FormLayout();
+        
+        TextField textField = new TextField("Foo");
+        formLayout.addComponent(textField);
+        textField = new TextField("Bar");
+        textField.setRequired(true);
+        textField.setComponentError(new UserError("This is error"));
+        formLayout.addComponent(textField);
+        
+        Switch switch1 = new Switch();
+        switch1.setCaption("Diipadaipa");
+        formLayout.addComponent(switch1);
+        
+        VerticalComponentGroup verticalComponentGroup = new VerticalComponentGroup("TouchKit form");
+        verticalComponentGroup.addComponent(formLayout);
+        
+        navigationView.setContent(verticalComponentGroup);
+        addComponent(navigationView);
+    }
+
+
+}
