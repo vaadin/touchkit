ae753b88ae672a25acf087fd437666d45fbc3c2a
Jonatan Kronqvist <jonatan.kronqvist@itmill.com>
2013-02-28 08:46:46 +0000

    Implemented a cache manifest status indicator, not yet programmatically configurable #9879
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25659 de684a11-831f-0410-ab27-cb86445eb682

---
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/offlinemode/CacheManifestStatusIndicator.java
@@ -0,0 +1,138 @@
+package com.vaadin.addon.touchkit.gwt.client.offlinemode;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * This is a simple application cache monitor. It also notifies demo users when
+ * offline mode is ready to be used.
+ */
+public class CacheManifestStatusIndicator implements EntryPoint {
+
+    private Element progressElement;
+    private CacheManifestStatusMessages messages;
+    private boolean shouldRequestUpdate = false;
+    private boolean updating;
+
+    public void onModuleLoad() {
+        new CacheManifestStatusIndicator().init();
+    }
+
+    public void init() {
+        messages = GWT.create(CacheManifestStatusMessages.class);
+        hookAllListeners(this);
+        scheduleUpdateChecker();
+    }
+
+    /**
+     * Check for updates to the application cache every 30 minutes
+     */
+    private void scheduleUpdateChecker() {
+        Scheduler.get().scheduleFixedPeriod(new Scheduler.RepeatingCommand() {
+            @Override
+            public boolean execute() {
+                if (!updating) {
+                    shouldRequestUpdate = true;
+                    updateCache();
+                }
+                return true;
+            }
+        }, 1800000);
+    }
+
+    protected void onCacheEvent(Event event) {
+        // consoleLog(event.getType());
+        if ("cached".equals(event.getType())) {
+            hideProgress();
+        } else if ("checking".equals(event.getType())) {
+            showProgress();
+        } else if ("downloading".equals(event.getType())) {
+            updating = true;
+            showProgress();
+        } else if ("noupdate".equals(event.getType())) {
+            hideProgress();
+        } else if ("updateready".equals(event.getType())) {
+            hideProgress();
+            if (shouldRequestUpdate) {
+                shouldRequestUpdate = false;
+                requestUpdate(false);
+            }
+            updating = false;
+        }
+    }
+
+    protected void onError(Event event) {
+        consoleLog("An error occurred");
+    }
+
+    protected void showProgress() {
+        if (progressElement == null) {
+            progressElement = Document.get().createDivElement();
+            progressElement.addClassName("v-cache-loading-indicator");
+        }
+        RootPanel.getBodyElement().appendChild(progressElement);
+    }
+
+    protected void hideProgress() {
+        if (progressElement != null) {
+            progressElement.removeFromParent();
+        }
+    }
+
+    protected void requestUpdate(boolean force) {
+        consoleLog("Requesting permission to update");
+        if (force || Window.confirm(messages.updateNowMsg())) {
+            Window.Location.reload();
+        }
+    }
+
+    protected final native void hookAllListeners(
+            CacheManifestStatusIndicator instance)
+    /*-{
+        $wnd.applicationCache.addEventListener('cached',
+            function(event) {
+                instance.@com.vaadin.addon.touchkit.gwt.client.offlinemode.CacheManifestStatusIndicator::onCacheEvent(Lcom/google/gwt/user/client/Event;)(event);
+            }, false);
+        $wnd.applicationCache.addEventListener('checking',
+            function(event) {
+                instance.@com.vaadin.addon.touchkit.gwt.client.offlinemode.CacheManifestStatusIndicator::onCacheEvent(Lcom/google/gwt/user/client/Event;)(event);
+            }, false);
+        $wnd.applicationCache.addEventListener('downloading',
+            function(event) {
+                instance.@com.vaadin.addon.touchkit.gwt.client.offlinemode.CacheManifestStatusIndicator::onCacheEvent(Lcom/google/gwt/user/client/Event;)(event);
+            }, false);
+        $wnd.applicationCache.addEventListener('noupdate',
+            function(event) {
+                instance.@com.vaadin.addon.touchkit.gwt.client.offlinemode.CacheManifestStatusIndicator::onCacheEvent(Lcom/google/gwt/user/client/Event;)(event);
+            }, false);
+        $wnd.applicationCache.addEventListener('updateready',
+            function(event) {
+                instance.@com.vaadin.addon.touchkit.gwt.client.offlinemode.CacheManifestStatusIndicator::onCacheEvent(Lcom/google/gwt/user/client/Event;)(event);
+            }, false);
+        $wnd.applicationCache.addEventListener('error',
+            function(event) {
+                instance.@com.vaadin.addon.touchkit.gwt.client.offlinemode.CacheManifestStatusIndicator::onError(Lcom/google/gwt/user/client/Event;)(event);
+            }, false);
+    }-*/;
+
+    private static native int getStatus()
+    /*-{
+        return $wnd.applicationCache.status;
+    }-*/;
+
+    protected static native void updateCache()
+    /*-{
+        return $wnd.applicationCache.update();
+    }-*/;
+
+    native void consoleLog(String message)
+    /*-{
+       console.log("CacheManifestStatusIndicator: " + message );
+    }-*/;
+}
---
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/offlinemode/CacheManifestStatusMessages.java
@@ -0,0 +1,11 @@
+package com.vaadin.addon.touchkit.gwt.client.offlinemode;
+
+import com.google.gwt.i18n.client.Constants;
+
+/**
+ * Localized / configurable messages for the
+ * {@link CacheManifestStatusIndicator}
+ */
+public interface CacheManifestStatusMessages extends Constants {
+    String updateNowMsg();
+}
