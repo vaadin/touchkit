6b7b0274f988299b84e319cd02eb8883ee7c4cab
Jonatan Kronqvist <jonatan.kronqvist@itmill.com>
2013-03-19 12:26:45 +0000

    Added the NavigationButton "pill" style to StyleNames #11146
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25712 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/theme/StyleNames.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/theme/StyleNames.java
@@ -4,7 +4,16 @@ package com.vaadin.addon.touchkit.gwt.client.theme;
  * This class contains special style variations available in TouchKits theme.
  */
 public interface StyleNames {
+    /** Makes a normal Button red. */
     public static final String BUTTON_RED = "red";
+    /** Makes a normal Button green. */
     public static final String BUTTON_GREEN = "green";
+    /** Makes a normal Button blue. */
     public static final String BUTTON_BLUE = "blue";
+
+    /**
+     * Makes the description in a NavigationButton look kind of like a pill. See
+     * MobileMail for an example.
+     */
+    public static final String NAVIGATION_BUTTON_DESC_PILL = "pill";
 }
