435c4ec941f981199266c1cf125c0e84fcc70115
Jonatan Kronqvist <jonatan.kronqvist@itmill.com>
2013-04-10 08:13:44 +0000

    Added missing style name constants #11146
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25812 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/theme/StyleNames.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/gwt/client/theme/StyleNames.java
@@ -1,13 +1,20 @@
 package com.vaadin.addon.touchkit.gwt.client.theme;
 
+import com.vaadin.ui.themes.BaseTheme;
+
 /**
- * This class contains special style variations available in TouchKits theme.
+ * This class contains special style variations available in the TouchKit theme.
  */
-public interface StyleNames {
+public class StyleNames extends BaseTheme {
+    // Overridden from BaseTheme
+    public static final String THEME_NAME = "touchkit";
+
     /** Makes a normal Button red. */
     public static final String BUTTON_RED = "red";
+
     /** Makes a normal Button green. */
     public static final String BUTTON_GREEN = "green";
+
     /** Makes a normal Button blue. */
     public static final String BUTTON_BLUE = "blue";
 
@@ -16,4 +23,39 @@ public interface StyleNames {
      * MobileMail for an example.
      */
     public static final String NAVIGATION_BUTTON_DESC_PILL = "pill";
+
+    /**
+     * Makes a navigation button gray and pointing forward.
+     */
+    public static final String NAVIGATION_BUTTON_FORWARD = "forward";
+
+    /**
+     * Makes a navigation button gray and pointing backward.
+     */
+    public static final String NAVIGATION_BUTTON_BACK = "back";
+
+    /**
+     * Sets an up arrow icon on a {@link com.vaadin.ui.Button} in a
+     * {@link com.vaadin.addon.touchkit.ui.NavigationBar}
+     */
+    public static final String NAVBAR_BUTTON_ARROW_UP = "icon-arrow-up";
+
+    /**
+     * Sets a down arrow icon on a {@link com.vaadin.ui.Button} in a
+     * {@link com.vaadin.addon.touchkit.ui.NavigationBar}
+     */
+    public static final String NAVBAR_BUTTON_ARROW_DOWN = "icon-arrow-down";
+
+    /**
+     * Sets a left arrow icon on a {@link com.vaadin.ui.Button} in a
+     * {@link com.vaadin.addon.touchkit.ui.NavigationBar}
+     */
+    public static final String NAVBAR_BUTTON_ARROW_LEFT = "icon-arrow-left";
+
+    /**
+     * Sets a right arrow icon on a {@link com.vaadin.ui.Button} in a
+     * {@link com.vaadin.addon.touchkit.ui.NavigationBar}
+     */
+    public static final String NAVBAR_BUTTON_ARROW_RIGHT = "icon-arrow-right";
+
 }
