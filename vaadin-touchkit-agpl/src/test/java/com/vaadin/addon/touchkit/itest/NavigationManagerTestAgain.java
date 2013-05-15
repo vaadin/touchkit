1b1c0e48142a8db10484f6150af2c11537147e3a
Matti Tahvonen <matti.tahvonen@itmill.com>
2013-05-15 16:08:40 +0000

    improved test case
    git-svn-id: http://dev.vaadin.com/svn/addons/TouchKit@25962 de684a11-831f-0410-ab27-cb86445eb682

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/NavigationManagerTestAgain.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/NavigationManagerTestAgain.java
@@ -1,33 +1,57 @@
 package com.vaadin.addon.touchkit.itest;
 
 import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
+import com.vaadin.addon.touchkit.ui.NavigationButton;
 import com.vaadin.addon.touchkit.ui.NavigationManager;
 import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.addon.touchkit.ui.Popover;
+import com.vaadin.addon.touchkit.ui.VerticalComponentGroup;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
 public class NavigationManagerTestAgain extends AbstractTouchKitIntegrationTest {
 
     public NavigationManagerTestAgain() {
         setDescription("Forward and backward navigation with normal buttons should work.");
         final NavigationManager manager = new NavigationManager();
-        addComponent(manager);
 
         final NavigationView first = new NavigationView();
         final NavigationView second = new NavigationView();
-
-        second.setContent(new Button("Go back", new Button.ClickListener() {
+        
+        second.setContent(new Label("Content"));
+        second.setLeftComponent(new Button("Finnish", new Button.ClickListener() {
             public void buttonClick(Button.ClickEvent event) {
-                manager.navigateBack();
+                final Popover popover = new Popover();
+                VerticalLayout l = new VerticalLayout();
+                l.setSpacing(true);
+                l.addComponent(new Button("Save and go back", new ClickListener() {
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        manager.navigateBack();
+                        popover.close();
+                    }
+                }));
+                l.addComponent(new Button("Cancel", new ClickListener() {
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        popover.close();
+                    }
+                }));
+                popover.setContent(l);
+                popover.showRelativeTo(event.getButton());
             }
         }));
 
-        first.setContent(new Button("Go to second level",
-                new Button.ClickListener() {
-                    public void buttonClick(Button.ClickEvent event) {
-                        manager.navigateTo(second);
-                    }
-                }));
+        NavigationButton c = new NavigationButton("Go to second level",second);
+        VerticalComponentGroup verticalComponentGroup = new VerticalComponentGroup();
+        verticalComponentGroup.addComponent(c);
+        first.setContent(verticalComponentGroup);
 
         manager.navigateTo(first);
+        
+        addComponent(manager);
     }
 }
