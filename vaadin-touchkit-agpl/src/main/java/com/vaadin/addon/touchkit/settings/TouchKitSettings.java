e219317438798be7401b150b74de2f480359fe74
Matti Tahvonen <matti@vaadin.com>
2013-08-29 17:06:09 +0300

    Fixed bad viewport size in WP8 devices
    With default rules (chosen for ios) viewport size is wrong in landscape
    mode in WP8. Setting combination that works for WP8, bugs again in iOS.
    Now there is a possibility to configure ViewPortSettings per request and
    IE10 gets settings that works properly in WP8. 
    
    Change-Id: I606c7365c98684e7a9c13d69bbf8aff99958688a

--- vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/settings/TouchKitSettings.java
+++ vaadin-touchkit-agpl/src/main/java/com/vaadin/addon/touchkit/settings/TouchKitSettings.java
@@ -1,5 +1,7 @@
 package com.vaadin.addon.touchkit.settings;
 
+import java.util.ArrayList;
+
 import com.vaadin.addon.touchkit.server.TouchKitServlet;
 import com.vaadin.server.BootstrapFragmentResponse;
 import com.vaadin.server.BootstrapListener;
@@ -24,7 +26,21 @@ import com.vaadin.server.VaadinService;
 public class TouchKitSettings implements BootstrapListener,
         SessionInitListener, SystemMessagesProvider {
 
-    private ViewPortSettings viewPortSettings;
+    /**
+     * Interface to select different settings for different kind of devices
+     * based on e.g. HTTP header inspection.
+     */
+    interface SettingSelector<T> {
+        /**
+         * @param response
+         * @return used settings or null if settings shouldn't be used for this
+         *         request.
+         */
+        T select(BootstrapPageResponse response);
+    }
+
+    private ViewPortSettings defaultViewPortSettings;
+    private ArrayList<SettingSelector<ViewPortSettings>> selectorBasedViewPortSettings = new ArrayList<TouchKitSettings.SettingSelector<ViewPortSettings>>();
     private WebAppSettings webAppSettings;
     private ApplicationIcons applicationIcons;
     private ApplicationCacheSettings applicationCacheSettings;
@@ -47,6 +63,33 @@ public class TouchKitSettings implements BootstrapListener,
      */
     public TouchKitSettings(VaadinService vaadinService) {
         setViewPortSettings(new ViewPortSettings());
+
+        /*
+         * WP8 needs device-width and maximum scale to work like ios (with
+         * default settings). Without these custom values pixel size is huge in
+         * landscape mode. With these values iphone5 goes to letter box when on
+         * home screen.
+         * 
+         * @viewport css rule will replace meta tags in the future and there is
+         * a prefixed version in MobileIE10, but it is buggy (landscape mode
+         * don't change "device-width").
+         */
+        final ViewPortSettings ieViewPortSettings = new ViewPortSettings();
+        ieViewPortSettings.setViewPortWidth("device-width");
+        ieViewPortSettings.setViewPortMaximumScale(1f);
+
+        addViewPortSettings(new SettingSelector<ViewPortSettings>() {
+
+            @Override
+            public ViewPortSettings select(BootstrapPageResponse response) {
+                String ua = response.getRequest().getHeader("User-Agent");
+                if (ua != null && ua.contains("IEMobile")) {
+                    return ieViewPortSettings;
+                }
+                return null;
+            }
+
+        });
         setWebAppSettings(new WebAppSettings());
         setApplicationIcons(new ApplicationIcons());
         setApplicationCacheSettings(new ApplicationCacheSettings());
@@ -56,10 +99,28 @@ public class TouchKitSettings implements BootstrapListener,
     }
 
     /**
+     * Selects viewport settings to be used for given bootstrap
+     * request/response.
+     * 
+     * @param response
      * @return The {@link ViewPortSettings}
      */
+    private ViewPortSettings selectViewPortSettings(
+            BootstrapPageResponse response) {
+        for (SettingSelector<ViewPortSettings> s : selectorBasedViewPortSettings) {
+            ViewPortSettings selected = s.select(response);
+            if (selected != null) {
+                return selected;
+            }
+        }
+        return defaultViewPortSettings;
+    }
+
+    /**
+     * @return The {@link ViewPortSettings} used by default.
+     */
     public ViewPortSettings getViewPortSettings() {
-        return viewPortSettings;
+        return defaultViewPortSettings;
     }
 
     /**
@@ -83,8 +144,9 @@ public class TouchKitSettings implements BootstrapListener,
 
     @Override
     public void modifyBootstrapPage(BootstrapPageResponse response) {
-        if (getViewPortSettings() != null)
-            getViewPortSettings().modifyBootstrapPage(response);
+        ViewPortSettings viewPortSettings2 = selectViewPortSettings(response);
+        if (viewPortSettings2 != null)
+            viewPortSettings2.modifyBootstrapPage(response);
         if (getWebAppSettings() != null)
             getWebAppSettings().modifyBootstrapPage(response);
         if (getApplicationIcons() != null)
@@ -127,13 +189,25 @@ public class TouchKitSettings implements BootstrapListener,
     }
 
     /**
-     * Sets the {@link ViewPortSettings} instance to use.
+     * Sets the {@link ViewPortSettings} instance to use by default.
      * 
      * @param viewPortSettings
      *            the {@link ViewPortSettings} instance to use.
+     * @see #addViewPortSettings(SettingSelector)
      */
     public void setViewPortSettings(ViewPortSettings viewPortSettings) {
-        this.viewPortSettings = viewPortSettings;
+        this.defaultViewPortSettings = viewPortSettings;
+    }
+
+    /**
+     * Adds a request dependent view port settings. If selector returns
+     * settings, it will override default view port setting.
+     * 
+     * @param viewPortSettingSelector
+     */
+    public void addViewPortSettings(
+            SettingSelector<ViewPortSettings> viewPortSettingSelector) {
+        selectorBasedViewPortSettings.add(viewPortSettingSelector);
     }
 
     /**
