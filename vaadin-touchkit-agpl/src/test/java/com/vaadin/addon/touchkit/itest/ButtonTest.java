3076d06a961b4f356c1b465395703fa747c77996
Matti Tahvonen <matti@vaadin.com>
2013-10-16 21:43:00 +0300

    Fixed button and notification font size and made notifications 100% w
    Change-Id: I84492231193aa42759edbd125d29a07d94721922

--- vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/ButtonTest.java
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/ButtonTest.java
@@ -5,29 +5,32 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.themes.BaseTheme;
 
-public class ButtonTest extends AbstractTouchKitIntegrationTest implements ClickListener {
+public class ButtonTest extends AbstractTouchKitIntegrationTest implements
+        ClickListener {
     private int i;
 
-	public ButtonTest() {
+    public ButtonTest() {
         setDescription("This is a button test");
         Button button = new Button();
         button.setCaption("button caption");
         button.addClickListener(this);
-        
+
         Button link = new Button();
         link.setCaption("link caption");
         link.setStyleName(BaseTheme.BUTTON_LINK);
         link.addClickListener(this);
-        
+
         addComponent(button);
         addComponent(link);
     }
 
     @Override
     public void buttonClick(ClickEvent event) {
-        Notification.show("Clicked " + ++i);
+        Notification.show("Clicked " + ++i, "Longer content for notification",
+                Type.ERROR_MESSAGE);
     }
 
 }
