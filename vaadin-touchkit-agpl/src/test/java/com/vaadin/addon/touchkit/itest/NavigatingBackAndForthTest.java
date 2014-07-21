eeaa18a505f341f433651ef55cb86e386dbddd2f
Juho Nurminen <juho@vaadin.com>
2014-07-21 18:35:36 +0300

    Fix icon styling in NavigationButton (#14245)
    Change-Id: Ibe90bb920ce77970eb4106b59c19e21a22163b3c

---
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/NavigatingBackAndForthTest.java
@@ -0,0 +1,38 @@
+package com.vaadin.addon.touchkit.itest;
+
+import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
+import com.vaadin.addon.touchkit.ui.NavigationButton;
+import com.vaadin.addon.touchkit.ui.NavigationButton.NavigationButtonClickEvent;
+import com.vaadin.addon.touchkit.ui.NavigationButton.NavigationButtonClickListener;
+import com.vaadin.addon.touchkit.ui.NavigationManager;
+import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.ui.Label;
+
+public class NavigatingBackAndForthTest extends AbstractTouchKitIntegrationTest {
+
+    public NavigatingBackAndForthTest() {
+        final NavigationManager m = new NavigationManager();
+
+        NavigationView view = new NavigationView("Initial");
+        view.setContent(new Label(
+                "Go next, then back, then next, should not fail"));
+        m.navigateTo(view);
+
+        final NavigationView another = new NavigationView("Another");
+
+        NavigationButton navigationButton = new NavigationButton("Next");
+        navigationButton.addClickListener(new NavigationButtonClickListener() {
+
+            @Override
+            public void buttonClick(NavigationButtonClickEvent event) {
+                m.navigateTo(another);
+            }
+        });
+
+        view.setRightComponent(navigationButton);
+
+        addComponent(m);
+
+    }
+
+}
