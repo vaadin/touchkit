9ca2979c9c3cb12312122069ead427aac8eabc0f
Juho Nurminen <juho@vaadin.com>
2015-03-27 13:28:54 +0200

    Fix scrolling in Chrome in UIs with a VerticalLayout and a Toolbar (#17251)
    Change-Id: Ice7b1c37a4235e60f65c4788d5117b520189a6f5

---
+++ vaadin-touchkit-agpl/src/test/java/com/vaadin/addon/touchkit/itest/VerticalLayoutScrolling.java
@@ -0,0 +1,47 @@
+package com.vaadin.addon.touchkit.itest;
+
+import com.vaadin.addon.touchkit.AbstractTouchKitIntegrationTest;
+import com.vaadin.addon.touchkit.ui.NavigationButton;
+import com.vaadin.addon.touchkit.ui.NavigationManager;
+import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.addon.touchkit.ui.Toolbar;
+import com.vaadin.addon.touchkit.ui.VerticalComponentGroup;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.VerticalLayout;
+
+public class VerticalLayoutScrolling extends AbstractTouchKitIntegrationTest {
+
+    public VerticalLayoutScrolling() {
+        setDescription("Displays a VerticalLayout inside a NavigationView to test scrolling and issue #17251");
+
+        final NavigationManager layout = new NavigationManager();
+        addComponent(layout);
+        layout.setSizeFull();
+
+        layout.setCurrentComponent(new NavigationTestView());
+    }
+
+    public static class NavigationTestView extends NavigationView {
+
+        public NavigationTestView() {
+
+            final VerticalLayout vl = new VerticalLayout();
+            vl.setMargin(true);
+            setContent(vl);
+
+            final VerticalComponentGroup g = new VerticalComponentGroup();
+            vl.addComponent(g);
+
+            for (int i = 0; i < 100; i++) {
+                g.addComponent(new NavigationButton("test"));
+            }
+
+            getNavigationBar().setCaption("List");
+            setToolbar(new Toolbar() {
+                {
+                    addComponent(new Button("dadasd"));
+                }
+            });
+        }
+    }
+}
